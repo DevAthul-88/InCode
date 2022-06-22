a7f8a04c1ae41510e8491e0309418cd6c1eadd00
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-06-22 11:36:41 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Router , Switch} from "wouter";
+import { Route, Router, Switch } from "wouter";
 import Home from "./pages/home";
 import Editor from "./pages/editor";
 import Start from "./pages/start";
@@ -9,18 +9,14 @@ import Notfound from "./pages/notfound";
 function App() {
   return (
     <Switch>
-      <Router>
         <Route path="/" component={Home} />
         <Route path="/editor/:id">
-          {(params) => <Editor id={params.id}/>}
+          {(params) => <Editor id={params.id} />}
         </Route>
         <Route path="/start" component={Start} />
         <Route path="/signin" component={SignIn} />
         <Route path="/signup" component={SignUp} />
-        <Route path="/:rest*">
-          <Notfound />
-        </Route>
-      </Router>
+        <Route path='/:rest*' component={Notfound} />
     </Switch>
   );
 }
